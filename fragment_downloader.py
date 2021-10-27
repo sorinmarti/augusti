86cf60dd56e88c14e16fc9139e26f29d6592cda0
Sorin Marti <32014438+sorinmarti@users.noreply.github.com>
2021-10-27 11:12:26 +0200

    Added comments and help text

--- fragment_downloader.py
+++ fragment_downloader.py
@@ -1,3 +1,17 @@
+"""This script allows a user to download images (or any files for that matter) which are listed in a
+comma separated file. The script allows to define a condition column and value which determines if a
+line in the csv file gets downloaded. The user can choose to ignore the first line and set an output
+directory for the downloaded images which will be created if it does not exist. File names are numbered
+by default but a user can set the filename-column to derive the filename from.
+Note: Column index starts at 0.
+
+Example: python fragment_downloader.py -i0 data.csv 3
+--> Downloads the urls saved in column 3 of the file 'data.csv' starting from the second line.
+
+Example: python fragment_downloader.py --delimiter ; --condition-column 4 --condition-value foo data.csv 3
+--> Downloads the urls saved in column 3 of the file 'data.csv' if the column 4 contains the value 'foo'.
+    The file is delimited by semicolons."""
+
 import argparse
 import csv
 import shutil
@@ -13,7 +27,7 @@ my_parser = argparse.ArgumentParser(prog='FragLoader',
 my_parser.add_argument('csv_file',
                        metavar='CSV-File',
                        type=str,
-                       help='the file to extract the download urls from')
+                       help='The CSV file to extract the download urls from')
 
 my_parser.add_argument('url_column',
                        metavar='URL-Column',
@@ -23,30 +37,31 @@ my_parser.add_argument('url_column',
 my_parser.add_argument('--delimiter',
                        default='\t',
                        type=str,
-                       )
+                       help='CSV-File delimiter. Default is \\t (TAB)')
 
 my_parser.add_argument('--condition-column',
                        default=-1,
                        type=int,
-                       )
+                       help='The column which contains the download condition value')
 
 my_parser.add_argument('--condition-value',
                        default='',
                        type=str,
-                       )
+                       help='The value which the condition-column must contain so the file gets downloaded.')
 
 my_parser.add_argument('--output-folder',
                        default='output',
                        type=str,
-                       )
+                       help='The folder to store the downloaded files to (default: output)')
 
 my_parser.add_argument('--file-name-column',
                        default=None,
                        type=str,
-                       )
+                       help='The column to derive the file name from')
 
 my_parser.add_argument('-i0', '--ignore-first-line',
-                       action='store_true')
+                       action='store_true',
+                       help='Ignores first line of csv file')
 
 # Execute the parse_args() method
 args = my_parser.parse_args()
@@ -95,7 +110,8 @@ if p.exists() and p.is_file():
                         if download_image:
                             r = requests.get(row[args.url_column], stream=True)
                             if r.status_code == 200:
-                                # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+                                # Set decode_content value to True, otherwise the
+                                # downloaded image file's size will be zero.
                                 r.raw.decode_content = True
                                 # Open a local file with wb ( write binary ) permission.
                                 prefix = ""
